6aff504e73cc5bd8205bc45d4d5b1448ea3a37dd
Esben Aarseth <Esben.Aarseth@bekk.no>
2016-10-22 10:17:24 +0200

    Oppdatert doughnut-chart.

--- js/statistics/SleepChart.react.jsx
+++ js/statistics/SleepChart.react.jsx
@@ -4,17 +4,16 @@ var restService = require('./RestService');
 var moment = require('moment');
 
 var data = {
-    datasets: [{
-        data: [
-            12,
-            12
-        ],
-        backgroundColor: [
-            "#df565b",
-            "#1a1a1a"
-        ],
-        label: 'Søvn vs våken tilstand'
-    }],
+    datasets: [
+        {
+            data: [],
+            backgroundColor: [
+                "#df565b",
+                "#1a1a1a"
+            ],
+            label: 'Søvn vs våken tilstand'
+        }
+    ],
     labels: [
         "Sover",
         "Våken"
@@ -30,11 +29,23 @@ module.exports = React.createClass({
     componentDidMount: function(){
         var that = this;
         restService.fetchSleep().then(function(sleeps){
-            
+            var firstSleep = moment(sleeps[0].start_time);
+            var now = new moment();
+            var difference = now.diff(firstSleep);
+            var sleepTime = sleeps.reduce(function(currentValue, nap) {
+                var startTime = moment(nap.start_time);
+                var endTime = moment(nap.end_time);
+                return difference - (endTime.diff(startTime));
+            }, difference);
+            var data = that.state.data;
+            data.datasets[0].data = [sleepTime, difference - sleepTime];
+            that.setState({
+                data: data
+            });
         });
     },
     render: function(){
-        if (this.state.data.labels.length === 0) {
+        if (this.state.data.datasets[0].data.length === 0) {
             return <div></div>;
         }
         return <Doughnut data={this.state.data} />
